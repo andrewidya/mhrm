5cf785b6d8fde3df51109f060f994cdf4497a58b
Andry UN <andrywidyaputra@gmail.com>
2019-02-27 14:59:54 +0700

    Add read environment variable for debug settings.

--- mhrm/settings.py
+++ mhrm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l8d*#w%nc9egz3s$bsqr-2=t@ao1ex!k&4(6jm4g0+%h-85k01'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
